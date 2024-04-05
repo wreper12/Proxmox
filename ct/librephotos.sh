978e9cd5c5fd0ddbc6043db09dc22dd17510b88d
Wreper12 <wreper12forminecraft@gmail.com>
2024-04-05 04:24:53 -0600

    Update librephotos.sh

--- ct/librephotos.sh
+++ ct/librephotos.sh
@@ -66,15 +66,15 @@ cd
 cd librephotos-docker
 
 msg_info "Stopping LibrePhotos"
-    docker compose down | cat
+    docker compose down &>/dev/null
 msg_ok "Stopped LibrePhotos"
 
 msg_info "Updating LibrePhotos"
-    docker compose pull | cat
+    docker compose pull &>/dev/null
 msg_ok "Updated LibrePhotos"
 
 msg_info "Starting LibrePhotos"
-    sudo docker compose up -d | cat
+    sudo docker compose up -d &>/dev/null
 msg_ok "Started LibrePhotos"
 
 exit
